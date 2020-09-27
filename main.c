b99120cf4ab31000511a306eb74aa6d12615abc4
Cristiano <neivacristiano@yahoo.com.br>
2020-09-27 18:56:51 -0300

    comments

--- main.c
+++ main.c
@@ -16,7 +16,11 @@
 #define SEMAPHORE_NAME "mysemaphore"
 #define NUM_STEPS 100000
 
-void closeSharedMemory()
+/*
+    to compile && run: gcc -Wall main.c -lrt  -o main -lpthread && ./main
+*/
+
+void closeSharedMemory() // fecha a memória compartilhada
 {
     if (shm_unlink(SHARED_MEMORY_NAME) == -1)
     {
@@ -25,13 +29,13 @@ void closeSharedMemory()
     }
 }
 
-void closeSemaphore(sem_t *semaphore)
+void closeSemaphore(sem_t *semaphore) // fechamento do semáforo
 {
     sem_close(semaphore);
     sem_unlink(SEMAPHORE_NAME);
 }
 
-void waitSums()
+void waitSums() // espera que todos os processos filhos tenham encerrado
 {
     pid_t pid;
 
@@ -44,19 +48,19 @@ void waitSums()
     }
 }
 
-void childWork(int *sharedMem, sem_t *semaphore)
+void childWork(int *sharedMem, sem_t *semaphore) // realiza o processo de soma coordenado pelo semáforo
 {
     for (int i = 0; i < NUM_STEPS; i++)
     {
-        sem_wait(semaphore);
+        sem_wait(semaphore);         // solicita acesso a area critica
         *sharedMem = *sharedMem + 1; // critical section
-        sem_post(semaphore);
+        sem_post(semaphore);         // libera area critica
     }
 }
 
-void parentWork(int *sharedMem)
+void parentWork(int *sharedMem) // espera os filhos processarem, imprime resultado e fecha a memoria compartilhada
 {
-    waitSums();
+    waitSums(); // aguarda encerramento do processamento
 
     int expected = NUM_PROCESS * NUM_STEPS;
 
@@ -64,10 +68,10 @@ void parentWork(int *sharedMem)
 
     printf("Expected and result are %s equal\n", expected == *sharedMem ? "" : "not");
 
-    closeSharedMemory();
+    closeSharedMemory(); // fecha memoria compartilhada
 }
 
-pid_t createProcesses(sem_t *semaphore)
+pid_t createProcesses(sem_t *semaphore) // cria novos processos
 {
     pid_t pid;
 
@@ -75,14 +79,14 @@ pid_t createProcesses(sem_t *semaphore)
     {
         pid = fork();
 
-        if (pid == 0)
+        if (pid == 0) // se for filho saia do loop
             break;
     }
 
     return pid;
 }
 
-int *createSharedMemory()
+int *createSharedMemory() // cria memoria compartilhada que armazenará a soma
 {
     const int SIZE = sizeof(int);
 
@@ -94,22 +98,24 @@ int *createSharedMemory()
 
     int *ptr = mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    *ptr = 0;
+    *ptr = 0; // inicializa soma com 0
 
     return ptr;
 }
 
 int main(int argc, char *argv[], char *envp[])
 {
-    int *sharedMem = createSharedMemory();
+    int *sharedMem = createSharedMemory(); // cria memoria
 
-    sem_t *semaphore = sem_open(SEMAPHORE_NAME, O_CREAT, 0644, 1);
+    //https://www.man7.org/linux/man-pages/man3/shm_open.3.html
 
-    pid_t pid = createProcesses(semaphore);
+    sem_t *semaphore = sem_open(SEMAPHORE_NAME, O_CREAT, 0644, 1); // cria o semaforo em uma memória compartilhada
+
+    pid_t pid = createProcesses(semaphore); // cria processos
 
     if (pid == 0)
-        childWork(sharedMem, semaphore);
+        childWork(sharedMem, semaphore); // soma se for filho
 
     else
-        parentWork(sharedMem);
+        parentWork(sharedMem); // resultado se for pai
 }
